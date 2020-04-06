2f97044e67746f2f1144ccf567f4cfd3888d07ae
Daniil <dan-trofimov@mail.ru>
2020-04-06 19:48:27 +0300

    Vue initial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <code-mirror/>
+  </div>
+</template>
+
+<script>
+import CodeMirror from '@/components/CodeMirror'
+
+export default {
+  name: 'App',
+  components: {
+    CodeMirror
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CodeMirror.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello there
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CodeMirror"
+    }
+</script>
+
+<style scoped>
+
+</style>
